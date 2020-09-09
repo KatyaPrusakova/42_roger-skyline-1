de316c7ac3a3bf0577e4e5746281ac25c5b6da44
Ekaterina Prusakova <eprusako@c1r1p3.hive.fi>
2020-09-09 11:24:21 +0300

    deployment draft

---
+++ deployment_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+cat=$'😻'
+cat=$'💩'
+
+
+echo -e "${grn}-----Done-----"
+echo -e "${mag}-----Done-----"
+echo -e "${cyn}-----Done-----"
+echo -e "${blu}-----Done-----"
+echo -e "${cat}"
